9c1f9f3cf7e5d7b1f97c76d9fead32f090d8afe3
Morita <168296824+Kainn1203@users.noreply.github.com>
2025-01-30 01:33:35 +0900

    add: careerの追加

--- js/script.js
+++ js/script.js
@@ -13,3 +13,20 @@ $(document).ready(function() {
 
     _window.trigger('scroll');
 });
+
+const hoverContent = document.querySelector('#hoverContent');
+const hoverImage = hoverContent.querySelector('img');
+const hoverText = document.querySelector('#hoverText');
+const yearContents = document.querySelectorAll('.yearContent');
+
+console.log(hoverText.textContent);
+
+yearContents.forEach((yearContent) => {
+    yearContent.addEventListener('mouseover', () => {
+        const yearImage = yearContent.querySelector('img');
+        const yearText = yearContent.querySelector('.yearText');
+        console.log(yearText.textContent);
+        hoverImage.src = yearImage.src;
+        hoverText.textContent = yearText.textContent;
+    })
+})
